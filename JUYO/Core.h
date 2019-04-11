9749dc2d93216c34423a2a21106428b26bfc9911
Andry Rafam Andrianjafy <41973268+AndryRafam@users.noreply.github.com>
2019-04-11 12:56:18 +0300

    Update Core.h

--- JUYO/Core.h
+++ JUYO/Core.h
@@ -14,9 +14,7 @@ class Core
 		Core(void); // Constructor
 		void Blowfish_Init (uchar *key, int keybytes);
 		void Blowfish_Encipher (uint32_t  *L, uint32_t *R);
-		void RC4A_Encrypt (uchar *plaintext, char *RC4A_key, uchar *ciphertext);
-		void RC4plus_Encrypt (uchar *plaintext, char *RC4plus_key, uchar *ciphertext);
-		void VMPC_Encrypt (uchar *plaintext, char *key, uchar *ciphertext);
+		void Encrypt (uchar *plaintext, char *key, uchar *ciphertext);
 		void *crack_text (uchar *text, uint32_t *Left, uint32_t *Right, int text_len);
 		uchar *Blowfish_key();
 		char *RC4A_key();
@@ -26,12 +24,9 @@ class Core
 
 	protected:
 		uint32_t F (uint32_t x);
-		void KSA_RC4A (char *key, uchar S[256]);
-		void KSA_RC4plus (char *key, uchar S[256]);
-		void KSA_VMPC (char *key, uchar S[256]);
+		void KSA (char *key, uchar S[256]);
 		void PRGA_RC4A (uchar S1[256], uchar S2[256], uchar *plaintext, uchar *ciphertext);
-		void PRGA_RC4plus (uchar S[256], uchar *plaintext, uchar *ciphertext);
-		void PRGA_VMPC (uchar S[256], uchar *plaintext, uchar *ciphertext);
+		void PRGA (uchar S[256], uchar *plaintext, uchar *ciphertext);
 
 };
 
