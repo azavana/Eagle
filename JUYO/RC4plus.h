012dd06bc62d7323c966e99461ae9338b5565e95
Andry Rafam Andrianjafy <41973268+AndryRafam@users.noreply.github.com>
2019-04-11 12:58:52 +0300

    Update RC4plus.h

--- JUYO/RC4plus.h
+++ JUYO/RC4plus.h
@@ -6,12 +6,12 @@
 class RC4plus: public Core
 {
 	public:
-		void RC4plus_Encrypt (uchar *plaintext, char *RC4plus_key, uchar *ciphertext);
+		void Encrypt (uchar *plaintext, char *RC4plus_key, uchar *ciphertext);
 		char *RC4plus_key();
 	
 	protected:
-		void KSA_RC4plus (char *key, uchar S[256]);
-		void PRGA_RC4plus (uchar S[256], uchar *plaintext, uchar *ciphertext);
+		void KSA (char *key, uchar S[256]);
+		void PRGA (uchar S[256], uchar *plaintext, uchar *ciphertext);
 };
 
 #endif
